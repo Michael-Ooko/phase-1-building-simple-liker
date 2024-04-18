37e5123e35855f56024ecf1d9992ee9ff5c13b2e
Michael <oliengo.michael@gmail.com>
2024-04-18 13:30:52 +0300

    working on lab

--- main.js
+++ main.js
@@ -1,11 +1,50 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+// Defining text characters for the empty and full hearts
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
-// Your JavaScript code goes here!
+// Select all elements with the class "like-glyph"
+const likeHearts = document.querySelectorAll('.like-glyph');
 
+// Attach event listener to each like heart
+likeHearts.forEach(likeHeart => {
+  likeHeart.addEventListener('click', (e) => {
+    const heart = e.target;
+    if (heart.classList.contains('activated-heart')) {
+      // Change heart back to empty
+      heart.textContent = EMPTY_HEART;
+      
+      // Remove activated-heart class
+      heart.classList.remove('activated-heart');
+    } else {
+      mimicServerCall()
+        .then(() => {
+          // Change heart to full and make it red on success
+          heart.textContent = FULL_HEART;
+          heart.classList.add('activated-heart');
+        })
+        .catch(() => {
+          // Display error modal on failure
+          displayError('Server Error');
 
+          // Hide modal after 3 seconds
+          setTimeout(() => {
+            const errorModal = document.querySelector('.modal');
+            errorModal.classList.add('hidden');
+          }, 3000);
+        });
+    }
+  });
+});
 
+function displayError(errorMessage) {
+  // Show the error modal
+  const errorModal = document.querySelector('.modal');
+  errorModal.classList.remove('hidden');
+  
+  // Set error message
+  const errorMessageElement = document.querySelector('#modal-message');
+  errorMessageElement.textContent = errorMessage;
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
